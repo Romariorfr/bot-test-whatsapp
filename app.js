f76d1b9d11a6325fc56efc0d8527b5d755402c5c
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-04-24 19:49:52 -0300

    projeto base

---
+++ app.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const token = process.env.WHATSAPP_TOKEN;
+
+const request = require("request"),
+  express = require("express"),
+  body_parser = require("body-parser"),
+  axios = require("axios").default,
+  app = express().use(body_parser.json());
+
+app.listen(process.env.PORT || 1337, () =>
+  console.log("webhook está escutando a porta...app v1.1")
+);
+
+app.post("/webhook", (req, res) => {
+  let body = req.body;
+
+  console.log("ola romario, esta é a versão 1.1");
+  console.log(JSON.stringify(req.body, null, 2));
+
+  if (req.body.object) {
+    if (
+      req.body.entry &&
+      req.body.entry[0].changes &&
+      req.body.entry[0].changes[0] &&
+      req.body.entry[0].changes[0].value.messages &&
+      req.body.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id =
+        req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = req.body.entry[0].changes[0].value.messages[0].from;
+      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
+      axios({
+        method: "POST",
+        url:
+          "https://graph.facebook.com/v12.0/" +
+          phone_number_id +
+          "/messages?access_token=" +
+          token,
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          text: { body: "Ack: " + msg_body },
+        },
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.get("/webhook", (req, res) => {
+  const verify_token = process.env.VERIFY_TOKEN;
+
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  if (mode && token) {
+    if (mode === "subscribe" && token === verify_token) {
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+});
